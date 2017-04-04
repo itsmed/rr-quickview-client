a469eedaf5a5d9348e115403d07092320e7514ab
DejonGill <dejonjgill@gmail.com>
2017-04-04 12:55:30 -0700

    init

---
+++ src/api-routes.js
@@ -0,0 +1,30 @@
+export const TestApiRouteBase = 'http://localhost:3000/api/';
+/**************************************
+  /users routes
+**************************************/
+
+
+// export const allUsersRoute = 'http://localhost:3000/api/users/all';
+// export const  = 'http://localhost:3000/api/users/id/';
+// export const  = 'http://localhost:3000/api/users/search/email/';
+// export const  = 'http://localhost:3000/api/users/search/name/';
+// export const  = 'http://localhost:3000/api/users/search/phone/';
+
+// /**************************************
+//   /transaction routes
+// **************************************/
+
+// export const  = 'http://localhost:3000/api/transactions/all';
+// export const  = 'http://localhost:3000/api/transactions/id/';
+// export const  = 'http://localhost:3000/api/transactions/user/id/';
+
+// /**************************************
+//   /employee routes
+// **************************************/
+// export const  = 'http://localhost:3000/api/employees/all';
+// export const  = 'http://localhost:3000/api/employees/id/';
+// export const  = 'http://localhost:3000/api/employees/search/name/';
+// export const  = 'http://localhost:3000/api/employees/search/permissions/';
+// export const  = 'http://localhost:3000/api/employees/search/email/';
+// export const  = 'http://localhost:3000/api/employees/search/phone/';
+// }
