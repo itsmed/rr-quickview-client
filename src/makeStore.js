96d593f3a0f76cf28c28b6edced019273830eaae
DejonGill <dejonjgill@gmail.com>
2017-04-18 09:51:24 -0700

    removed unnecessary debugging line

--- src/makeStore.js
+++ src/makeStore.js
@@ -7,7 +7,6 @@ import rootReducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
   )
 );
