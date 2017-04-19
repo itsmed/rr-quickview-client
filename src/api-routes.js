22ba7bc4f675933fe661182d260b55ebc357e286
DejonGill <dejonjgill@gmail.com>
2017-04-19 09:45:43 -0700

    change to const name, makes more sense

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,4 +1,4 @@
-export const TestApiRouteBase = 'http://localhost:3000/api/';
+export const API_ROUTE_BASE = 'http://localhost:3000/api/';
 /**************************************
   /users routes
 **************************************/
